6920ab707ed16c0346deab1f360245639ed830e7
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2021-12-11 20:05:01 -0300

    add progresso de filter

--- ftable.lua
+++ ftable.lua
@@ -1,68 +1,22 @@
 
 local ftable = {}
 
-    function ftable.array(_table)
-        
-        local class_array = setmetatable(
-            {_table},
-            {__index = 
-                {
-                    get_table = function(self)
-                        return self._table
-                    end,
+    ftable.filter = function(_array, _callback)
 
-                    set_table = function(self, _table)
-                        self._table = _table
-                    end,
+        local new_array = {}
+        local value_temp = nil
 
-                    map = function(self, _callback)
-
-                    end,
-
-                    filter = function(self, _callback)
-                        local filter_table = {}
-
-                        for i=1, #self._table do
-                            if _callback(_table[i]) then
-                                table.insert(filter_table, _table[i])
-                            end
-                        end
-
-                        return filter_table
-                    end
-                }
-            }
-        )
-
-        class_array._table = _table or nil
-
-        return class_array
-    end
-
-    function ftable.map(_table, _callback)
-
-        local map_table = {}
-
-        for i=1, #_table do
-            if _table[i] ~= nil then
-                table.insert(map_table, _callback(_table[i]))
+        for index, value in ipairs(_array) do
+            
+            if _callback(value) then
+                -- criar copias dos dados recebidos
+                -- value_temp = 
+                -- table.insert(new_array, value_temp)
             end
+
         end
-
-        return map_table
-    end
-
-    function ftable.filter(_table, _callback)
         
-        local filter_table = {}
-
-        for i=1, #_table do
-            if _callback(_table[i]) then
-                table.insert(filter_table, _table[i])
-            end
-        end
-
-        return filter_table
+        return new_array
     end
 
 return ftable
